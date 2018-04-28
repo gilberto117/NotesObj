c6861870a1bc4a912ed7a0f28f5f9634acaac49d
gilberto117 <gilberto.e.barrera@gmail.com>
2018-04-27 19:34:29 -0500

    primer commit

---
+++ NotasObj/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  NotasObj
+//
+//  Created by Gilberto Escareño Barrera on 26/04/18.
+//  Copyright © 2018 Gilberto Escareño Barrera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *txtUsuario;
+@property (weak, nonatomic) IBOutlet UITextField *txtContraseña;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _txtContraseña.text = @"Hola";
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)txtUsuario:(UITextField *)sender {
+}
+
+
+@end
+
---
+++ NotasObj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NotasObj
+//
+//  Created by Gilberto Escareño Barrera on 26/04/18.
+//  Copyright © 2018 Gilberto Escareño Barrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
